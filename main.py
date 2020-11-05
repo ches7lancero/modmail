7a9a56a8eeaa197d7664889ea8dfad9a23f64add
CTK blobby#5340 <58012793+ches7lancero@users.noreply.github.com>
2020-11-05 17:10:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+from discord.ext import commands
+import discord
+import os
 
+intents = discord.Intents.default()
+# we need members inyent too
+intents.members = True
+
+bot = commands.Bot(command_prefix = "!", intents = intents)
+
+@bot.event
+async def on_ready():
+  print("The bot is ready!")
+  bot.load_extensions("cogs.onMessage")
+  
+bot.run(os.environ.get("TOKEN"))
